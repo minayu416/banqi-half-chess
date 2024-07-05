75003fdcaeb08c3f47bf991e91eb0a78e6c9d37b
Ming-Jung YU <minayu416@gmail.com>
2024-07-05 19:18:30 +0800

    removed react dnd and using dnd kit

--- app/[game]/page.js
+++ app/[game]/page.js
@@ -1,17 +1,12 @@
 "use client"
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-
-import { DragPreviewImage, useDrag } from 'react-dnd'
-
-import { ItemTypes } from './type';
-
-import Header from "../component";
+import { DndContext, DragEndEvent, useDraggable, useDroppable } from '@dnd-kit/core';
 
 import { ChessShuffleHandler } from "./component"
+
+import Header from "../component";
 
 // TODO: Note: 必須要登入Google 帳號才能授權把資料即時更新到google/firestore
 
@@ -24,59 +19,28 @@ import { ChessShuffleHandler } from "./component"
 //              4. 注意的是隨機的棋子最後是蓋牌，不能存在客戶端被看到 (但這之後再處理，可能要random後直接存在後端或者firestore)
 
 // 可能要在這一層render 不同的棋子
-const Piece = ({chess}) => {
-  const [{ isDragging }, drag, preview] = useDrag(
-    () => ({
-      type: ItemTypes.KNIGHT,
-      collect: (monitor) => ({
-        isDragging: !!monitor.isDragging(),
-      }),
-    }),
-    [],
-  )
-    return (
-      <div ref={drag} key={chess.sn} 
+function Draggable(props) {
+
+  return (      
+  <div key={props.children.sn} 
       className={`absolute rounded-full w-20 h-20 drop-shadow-lg flex justify-center items-center`} 
-      style={{ backgroundColor: "#F1D6AE", opacity: isDragging ? 0.5 : 1, }}>
-   <div className="rounded-full w-[4.5rem] h-[4.5rem] border-2 flex justify-center items-center" style={{ borderColor: chess.color }}>
-       <p className="text-5xl lxgw-wenkai-tc-regular select-none" style={{ color: chess.color }}>{ chess.chineseName }
-       </p>
-   </div>
- </div>
-    )
-  
+      style={{ backgroundColor: "#F1D6AE" }}>
+    <div className="rounded-full w-[4.5rem] h-[4.5rem] border-2 flex justify-center items-center" style={{ borderColor: props.children.color }}>
+      <p className="text-5xl lxgw-wenkai-tc-regular select-none" style={{ color: props.children.color }}>{ props.children.chineseName }
+      </p>
+    </div>
+  </div>
+  );
 }
 
-const BoardSquare = ({ pos, children, game }) => {
-
-  // TODO: 判斷可否拖曳的function
-
-  // TODO: 每動一步 update setChessPosition
-
-  // 這一層去判斷可不可以drop -> 帶入位置、以及 game rule 跟 move func 去判斷
-  // const [{ isOver, canDrop }, drop] = useDrop(
-  //   () => ({
-  //     accept: ItemTypes.KNIGHT,
-  //     canDrop: () => game.canMoveKnight(x, y),
-  //     drop: () => game.moveKnight(x, y),
-  //     collect: (monitor) => ({
-  //       isOver: !!monitor.isOver(),
-  //       canDrop: !!monitor.canDrop(),
-  //     }),
-  //   }),
-  //   [game],
-  // )
+function DroppableCell(props) {
 
   return (
-    // ref={drop}
-    <div className="relative w-full h-full border flex justify-center items-center" style={{ borderColor: "#3C3B3B" }}>
-      {/* child 應該就是 chess */}
-      {children}
-      {/* {isOver && !canDrop && <Overlay type={OverlayType.IllegalMoveHover} />}
-      {!isOver && canDrop && <Overlay type={OverlayType.PossibleMove} />}
-      {isOver && canDrop && <Overlay type={OverlayType.LegalMoveHover} />} */}
+    <div key={props.position} className="relative w-full h-full border flex justify-center items-center" style={{ borderColor: "#3C3B3B" }}>
+      {props.children}
     </div>
   )
+
 }
 
 function Board() {
@@ -92,22 +56,55 @@ function Board() {
       setIsLoading(false);
    }, []); 
 
-  const divs = Array.from({ length: 32 }, (_, index) => (
+   function handleDragEnd(event) {
+
+  }
+
+  const divs = shuffledChess.map((chess, index) => (
     // 這是方格, 先暫時用index 給格子編號
-      <div key={index}>
-        {/*  */}
-        <BoardSquare pos={index}>
-          {/* 這個會根據render的位置去判斷放哪個棋、先仔細研究在寫 */}
-          {/* 用變動的dictionary去寫 */}
-          {/* 1. 先改資料結構、在寫進來，而不要寫死isKnight */}
-          <Piece chess={shuffledChess[index]} />
-        </BoardSquare>
-      </div>
+      <DroppableCell key={index} chess={chess}>
+        <div key={index} className="relative w-full h-full border flex justify-center items-center" style={{ borderColor: "#3C3B3B" }}>
+        {/* 棋子 */}
+        {chess == '.' ? (
+            <div>{null}</div>
+        ) : (
+          <Draggable key={index} chess={chess}>
+          <div key={chess.sn} 
+              className={`absolute rounded-full w-20 h-20 drop-shadow-lg flex justify-center items-center`} 
+              style={{ backgroundColor: "#F1D6AE" }}>
+            <div className="rounded-full w-[4.5rem] h-[4.5rem] border-2 flex justify-center items-center" style={{ borderColor: chess.color }}>
+              <p className="text-5xl lxgw-wenkai-tc-regular select-none" style={{ color: chess.color }}>{ chess.chineseName }
+              </p>
+            </div>
+          </div>
+          </Draggable>
+        )}
+        </div>
+      </DroppableCell>
     ));
     return (
+      <DndContext onDragEnd={handleDragEnd}>
       <div className="w-full h-full grid grid-cols-8">
-        {isLoading ? <></> : divs}
+        {shuffledChess.map((chess, index) => {
+          return (
+          // 這是方格, 先暫時用index 給格子編號
+            <DroppableCell key={index} position={index} chess={chess}>
+              {/* <div key={index} className="relative w-full h-full border flex justify-center items-center" style={{ borderColor: "#3C3B3B" }}> */}
+              {/* 棋子 */}
+              {chess == '.' ? (
+                  <div>{null}</div>
+              ) : (
+                <Draggable position={index} name={chess}>
+                  {chess}
+                </Draggable>
+              )}
+              {/* </div> */}
+            </DroppableCell>
+          )
+          })
+        }
       </div>
+      </DndContext>
     )
 }
 
@@ -123,7 +120,7 @@ function GameSection({}){
       {/* TODO Banqi bg another option: #9C836A*/}
         <div className="h-3/5 w-full border rounded-md p-3" style={{backgroundColor: "#96602E", borderColor: "#C18859"}}>
             <div className="w-full h-full border-2 rounded-md" style={{borderColor: "#3C3B3B"}}>
-              <Board/>
+              <Board />
             </div>
         </div>
       <div className="m-auto w-1/3 border flex" style={{backgroundColor: "#FFFBF8", borderColor: "#B59376"}}>
@@ -150,13 +147,11 @@ export default function Page({ params }) {
 
     return (
       <>
-      <DndProvider backend={HTML5Backend}>
         <Header/>
         <div className="min-h-screen py-24 px-12 flex w-full">
           <GameSection/>
           <ChatRoom/>
         </div>
-      </DndProvider>
       </>
       )
 }
