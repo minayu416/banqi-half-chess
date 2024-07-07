73c932ac814217119fb030ab7434aa08e33b5cc1
Ming-Jung YU <minayu416@gmail.com>
2024-07-07 17:34:28 +0800

    develoepd hone page

--- app/component.js
+++ app/component.js
@@ -1,9 +1,20 @@
 
 
-export default function Header() {
+export function Header() {
     return (
         <div className="fixed inset-x-0 top-0 h-16" style={{backgroundColor: "#B59376",}}>
 
         </div>
     );
   }
+
+
+export function generateRandomCode() {
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+let randomCode = '';
+for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomCode += characters[randomIndex];
+}
+return randomCode;
+}
