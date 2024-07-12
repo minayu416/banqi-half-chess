bb696b1186d03c78f0832821ff1cdb1ca7e7323a
Ming-Jung YU <minayu416@gmail.com>
2024-07-12 16:46:33 +0800

    finished turning animation and by turn

--- app/[game]/component.js
+++ app/[game]/component.js
@@ -1,5 +1,18 @@
+export const chessStyle = {
+    "b": {
+        "color": "#000000",
+        "king": "將",
+        "word": "黑方"
+    },
+    "r": {
+        "color": "#CC0000",
+        "king": "帥",
+        "word": "紅方"
+    }
+}
+
 const chessWordMapping = {
-    0: {
+    "b": {
         "borderColor": "",
         "king": "將",
         "advisor": "士",
@@ -9,7 +22,7 @@ const chessWordMapping = {
         "cannon": "砲",
         "soldier": "卒"
     },
-    1: {
+    "r": {
         "borderColor": "",
         "king": "帥",
         "advisor": "仕",
@@ -22,8 +35,8 @@ const chessWordMapping = {
 }
 
 const banqiGeneratePolicy = {
-    // black: 0, red: 1
-    "colors": [0, 1],
+    // black: b, red: r
+    "colors": ["b", "r"],
     "chessOptions": {
         "king": 1,
         "advisor": 2,
@@ -57,7 +70,7 @@ export const ChessGenerator = () => {
             for (let i = 0; i < chessOptions[chess]; i++) {
                 banqiGeneratedPool.push({
                     sn: `${sn}-${i}`,
-                    color: color === 0 ? "#000000" : "#CC0000",
+                    color: color === "b" ? "#000000" : "#CC0000",
                     type: chess,
                     weight: banqiWeight[chess],
                     chineseName: chessWordMapping[color][chess]
@@ -88,8 +101,7 @@ export const ChessShuffleHandler = () => {
         shuffledChess.push({
             position: i++,
             sn: chess.sn,
-            // TODO: 先假設是翻開的。
-            turned: true,
+            turned: false,
             color: chess.color,
             type: chess.type,
             weight: chess.weight,
--- app/[game]/page.js
+++ app/[game]/page.js
@@ -4,11 +4,13 @@ import React, { useEffect, useState, useMemo } from 'react';
 
 import { DndContext, DragEndEvent, useDraggable, useDroppable } from '@dnd-kit/core';
 
-import { ChessShuffleHandler } from "./component"
+import { chessStyle, ChessShuffleHandler } from "./component"
 
 import { Header } from "../component";
 import { ChessRules } from './rules';
-import { rule } from 'postcss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFlag } from '@fortawesome/free-solid-svg-icons'
 
 // TODO: Note: 必須要登入Google 帳號才能授權把資料即時更新到google/firestore
 
@@ -52,15 +54,25 @@ function Draggable(props) {
     {props.chess == '.' ? (
       <div>{null}</div>
   ) : ( 
-    <div ref={setNodeRef}
+    <div 
+    ref={setNodeRef}
+    // ref={props.currentUser === props.sequence ? setNodeRef : null}
     key={props.children.sn} 
     className={`absolute rounded-full w-20 h-20 drop-shadow-lg flex justify-center items-center`} 
     style={{ backgroundColor: "#F1D6AE", ...style }}
     {...listeners} {...attributes}>
-    <div className="rounded-full w-[4.5rem] h-[4.5rem] border-2 flex justify-center items-center" style={{ borderColor: props.children.color }}>
-      <p className="text-5xl lxgw-wenkai-tc-regular select-none" style={{ color: props.children.color }}>{ props.children.chineseName }
-      </p>
+    {!props.children.turned?
+    // 蓋牌
+    <div className="rounded-full w-[4.5rem] h-[4.5rem] flex justify-center items-center">
+      {/* <p className="text-5xl lxgw-wenkai-tc-regular select-none" style={{ color: props.children.color }}>{ props.children.chineseName }
+      </p> */}
     </div>
+    :
+      <div className="transition-transform ease-in-out duration-500 rounded-full w-[4.5rem] h-[4.5rem] border-2 flex justify-center items-center" style={{ borderColor: props.children.color, transform: 'rotateY(180deg)' }}>
+        <p className="text-5xl lxgw-wenkai-tc-regular select-none" style={{ color: props.children.color, transform: 'rotateY(180deg)' }}>{ props.children.chineseName }
+        </p>
+      </div>
+      }
   </div>
   )}  
 </>
@@ -87,7 +99,9 @@ function DroppableCell(props) {
   };
 
   return (
-    <div key={props.position} ref={setNodeRef} 
+    <div key={props.position} 
+    ref={setNodeRef}
+    // ref={props.currentUser === props.sequence ? setNodeRef : null}
     className="relative w-full h-full border flex justify-center items-center" 
     style={{ borderColor: "#3C3B3B", ...style  }}>
       {props.children}
@@ -96,15 +110,10 @@ function DroppableCell(props) {
 
 }
 
-function Board() {
+function Board({currentUser, opponent, side, setSide, sequence, changeSequence, setEventInfo}) {
   const [shuffledChess, setShuffledChess] = useState([]);
 
-  const [eventInfo, setEventInfo] = useState('<>');
-
-  // TODO: 紀錄誰是哪個顏色
-  const [side, setSide] = useState();
-  // TODO: 紀錄順序
-  const [sequence, setSequence] = useState();
+  // const [eventInfo, setEventInfo] = useState('<>');
 
   useEffect(() => {
       // 第一次 load 時先random 棋子、但未來要改成存進localStorage+更新firestore 以防止使用者F5刷新
@@ -119,7 +128,8 @@ function Board() {
     updatedChess[activeData.position] = '.';
     updatedChess[overData.position] = activeData.chess;
     setShuffledChess(updatedChess);
-    setEventInfo(`${String(event?.active?.id) ?? ''} -> ${String(event?.over?.id) ?? ''}`);
+    changeSequence(currentUser);
+    setEventInfo(`[${String(chessStyle[activeData.chess.sn[0]].word) ?? ''}] ${String(activeData.chess.chineseName) ?? ''} -> [${String(chessStyle[overData.chess.sn[0]].word) ?? ''}] ${String(overData.chess.chineseName) ?? ''}`);
   }
 
   function handleDragEnd(event) {
@@ -132,7 +142,37 @@ function Board() {
 
     let activeData = activeEvent.data.current;
     let overData = overEvent.data.current;
-    
+
+    if (!activeData.chess.turned){
+      const updatedChess = [...shuffledChess];
+      const turnedChess = {...activeData.chess}
+      turnedChess.turned = true;
+      updatedChess[activeData.position] = turnedChess;
+      setShuffledChess(updatedChess);
+      changeSequence(currentUser);
+      setEventInfo(`Turned on [${String(chessStyle[activeData.chess.sn[0]].word) ?? ''}] ${String(activeData.chess.chineseName) ?? ''}`);
+
+      if (side[currentUser] === null){
+        let newSide = {...side}
+        newSide[currentUser] = activeData.chess.sn[0];
+        const opponentSide = activeData.chess.sn[0] === "b" ? "r" : "b"
+        newSide[opponent] = opponentSide;
+        setSide(newSide)
+        changeSequence(currentUser);
+        setEventInfo('先攻方選定顏色');
+        // TODO: 存在雲端資料庫
+        return;
+      }
+      return;
+
+    }
+
+    // TODO: 你拿的不是自己的棋子 - 到時候單人模式跟雙人模式的邏輯要分開
+    if(activeData.chess.sn[0] !== side[sequence]){
+      setEventInfo('You took incorrect side chess.');
+      return;
+    }
+
     if(rules.isMoveSamePlace(activeData, overData)){
       setEventInfo('<>');
       return;
@@ -142,12 +182,13 @@ function Board() {
     if (rules.isMoveToEmptyPlace(activeData, overData)) {
       // moving to another empty place
       emitChange(activeData, overData)
+      setEventInfo('Moved chess to empty place');
       return;
     }
 
     // 同個棋子不能吃
     if (rules.isSameSide(activeData, overData)) {
-      setEventInfo('same side');
+      setEventInfo('You can not eat same color chess');
       return;
     }
 
@@ -157,10 +198,16 @@ function Board() {
     }
 
     // TODO: 可以隔很多格跳嗎？
-    if (rules.isCannonCanCommit(activeData, overData)){
+    if (rules.isCannon(activeData)){
         // x axis: abs(over.position - current.position) === 2
-        // y axis: abs(over.position - current.position) === 16
-        emitChange(activeData, overData)
+        // y axis: abs(over.position - current.position) === (16 or 24)
+        // 要判斷中間只能有一個格子
+        // && (Math.abs(overChess.position - currentChess.position) === 2 || Math.abs(overChess.position - currentChess.position) === 16)
+        if ((Math.abs(overData.position - activeData.position) === 2 || Math.abs(overData.position - activeData.position) === 16)){
+          emitChange(activeData, overData)
+          return;
+        }
+        return;
     }
 
     // Except cannon, other chess can not move over 1 step.
@@ -191,13 +238,13 @@ function Board() {
           return (
           // 這是方格, 先暫時用index 給格子編號
           // TODO: 邏輯要優化
-            <DroppableCell key={index} position={index} chess={chess}>
+            <DroppableCell key={index} position={index} chess={chess} currentUser={currentUser} sequence={sequence}>
               {/* <div key={index} className="relative w-full h-full border flex justify-center items-center" style={{ borderColor: "#3C3B3B" }}> */}
               {/* 棋子 */}
               {chess == '.' ? (
                   <div>{null}</div>
               ) : (
-                <Draggable position={index} chess={chess}>
+                <Draggable position={index} chess={chess} currentUser={currentUser} sequence={sequence}>
                   {chess}
                 </Draggable>
               )}
@@ -212,32 +259,94 @@ function Board() {
     )
 }
 
-function GameSection({}){
+function GameSection({setEventInfo}){
+    // TODO: 紀錄誰是哪個顏色、應該只需要用第一次
+    const [side, setSide] = useState({});
+    // TODO: 紀錄順序, TODO: 不能寫死，先攻是Game creator
+    // TODO: 動完每一步都要更新sequence 同步到資料庫
+    const [sequence, setSequence] = useState(null);
+
+    // TODO: 判斷當前使用者是誰、只存在客戶端做為辨別
+    const [currentUser, setCurrentUser] = useState("");
+    const [opponent, setOpponent] = useState("")
+  
+    useEffect(() => {
+      // 模擬線上串流、使用者加入的情況
+      // TODO: 跟網路資料庫串流、記得確認固定使用者、確認刷新時不會跑掉
+      // TODO: 先加入先翻
+      const me = "@mina22336"
+      const creator = me
+      const opponentSide = "@robot18732"
+      setCurrentUser(me)
+      setOpponent(opponentSide)
+      setSequence(creator);
+      
+      let newSide = {
+        [me]: null,
+        [opponentSide]: null
+      }
+      setSide(newSide);
+   }, []); 
+
+  function changeSequence(){
+      if (currentUser === sequence){
+        setSequence(opponent)
+        // TODO: 存 Firestore
+      } else {
+        setSequence(currentUser)
+      }
+  }
+ 
   return (
 
     <div className="w-2/3 flex flex-col justify-center items-center">
-    <div className="m-auto w-1/3 border flex" style={{backgroundColor: "#FFFBF8", borderColor: "#B59376"}}>
-      <div className='p-5'>照片</div>
-      <div className='p-5'>對手</div>
+
+    <div className={`m-auto w-2/4 border border flex justify-center items-center`} style={{backgroundColor: "#FFFBF8", borderColor: "#B59376"}}>
+      <div className='px-2'>照片</div>
+      <div className='px-5'>{opponent}</div>
+      <div className='py-2 m-auto'>
+        {side[opponent] &&
+      <div className={`rounded-full p-1 flex justify-center items-center`} style={{ backgroundColor: "#F1D6AE", borderColor: "#B59376" }}>
+        <div className="rounded-full px-1 border-2 flex justify-center items-center" style={{ borderColor: chessStyle[side[opponent]].color }}>
+          <p className="text-3xl lxgw-wenkai-tc-regular select-none" style={{ color: chessStyle[side[opponent]].color }}>{ chessStyle[side[opponent]].king }
+          </p>
+        </div>
+      </div>
+      }
+        </div>
+      <div>{ currentUser !== sequence && <FontAwesomeIcon icon={faFlag} size="xl" className="p-4" style={{color: "#FFD43B"}} /> }</div>
       </div>
       {/* TODO Banqi bg another option: #9C836A*/}
         <div className="h-3/5 w-full border rounded-md p-3" style={{backgroundColor: "#96602E", borderColor: "#C18859"}}>
             <div className="w-full h-full border-2 rounded-md" style={{borderColor: "#3C3B3B"}}>
-              <Board />
+              <Board currentUser={currentUser} opponent={opponent} side={side} setSide={setSide} sequence={sequence} changeSequence={changeSequence} setEventInfo={setEventInfo}/>
             </div>
         </div>
-      <div className="m-auto w-1/3 border flex" style={{backgroundColor: "#FFFBF8", borderColor: "#B59376"}}>
+      <div className={`m-auto w-2/4 border flex justify-center items-center`} style={{backgroundColor: "#FFFBF8", borderColor: "#B59376"}}>
       <div className='p-5'>照片</div>
-      <div className='p-5'>我</div>
+      <div className='p-5'>{currentUser}</div>
+      <div className='p-1 m-auto'>
+      {side[currentUser] &&
+      <div className={`rounded-full p-1 flex justify-center items-center`} style={{ backgroundColor: "#F1D6AE", borderColor: "#B59376" }}>
+        <div className="rounded-full px-1 border-2 flex justify-center items-center" style={{ borderColor: chessStyle[side[currentUser]].color }}>
+          <p className="text-3xl lxgw-wenkai-tc-regular select-none" style={{ color: chessStyle[side[currentUser]].color }}>{ chessStyle[side[currentUser]].king }
+          </p>
+        </div>
+        </div>
+      }
+        </div>
+        <div>{ currentUser === sequence && <FontAwesomeIcon icon={faFlag} size="xl" className="p-4" style={{color: "#FFD43B"}} /> }</div>
+      
         </div>
       </div>
   )
 
 }
 
-function ChatRoom({}){
+function ChatRoom({eventInfo}){
  return (
   <div className="w-1/3 flex flex-col justify-center items-center">
+    {eventInfo}
   {/* TODO implement chat room, refer Fancy chat room */}
   <div className="h-2/3 w-4/5 border rounded-md" style={{backgroundColor: "#FFFBF8", borderColor: "#B59376"}}>
   </div>
@@ -248,12 +357,14 @@ function ChatRoom({}){
 
 export default function Page({ params }) {
 
+  const [eventInfo, setEventInfo] = useState('<>');
+
     return (
       <>
         <Header/>
         <div className="min-h-screen py-24 px-12 flex w-full">
-          <GameSection/>
-          <ChatRoom/>
+          <GameSection setEventInfo={setEventInfo}/>
+          <ChatRoom eventInfo={eventInfo}/>
         </div>
       </>
       )
