c35377c656b26f300d47951f4c0615ac7b58cd05
Ming-Jung YU <minayu416@gmail.com>
2024-07-14 00:56:18 +0800

    finished ga,e rules

--- app/[game]/rules.js
+++ app/[game]/rules.js
@@ -1,10 +1,10 @@
 
 export class ChessRules {
     isMoveSamePlace = (currentChess, overChess) => currentChess.position === overChess.position; 
-    isSameSide = (currentChess, overChess) => currentChess.chess.sn[0] == overChess.chess.sn[0]
-    isMoveToEmptyPlace = (currentChess, overChess) => overChess.chess === "." && (currentChess.position !== overChess.position)
+    isSameSide = (currentChess, overChess) => overChess.chess !== "." && (currentChess.chess.sn[0] == overChess.chess.sn[0])
+    isMoveToEmptyPlace = (overChess) => overChess.chess === "."
     isCannon = (currentChess) => currentChess.chess.type === "cannon"
-    isTurned = (overChess) => overChess.chess.turned
+    // isTurned = (overChess) =>  overChess.chess.turned
     isOverStep = (currentChess, overChess) => Math.abs(overChess.position - currentChess.position) !== 1 && Math.abs(overChess.position - currentChess.position) !== 8
     isSolderCanCommit = (currentChess, overChess) => currentChess.chess.type === "soldier" && (overChess.chess.weight === 1 || overChess.chess.weight === 9)
     isKingCanCommit = (currentChess, overChess) => currentChess.chess.type === "king" && overChess.chess.weight === 1
